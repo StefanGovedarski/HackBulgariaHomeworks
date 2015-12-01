d01924d6ca236671ce4f843b3dcf44a36f10aad9
Stefan <stefan_govedarski@abv.bg>
2015-12-01 23:40:04 +0200

    Uploading HackBg Task from week 3

---
+++ HackTasksWeek3/Tuesday/W3D1_Time/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackWeek3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DateTime date  = new DateTime(2015,5,30,3,00,00);
+            Time time = new Time(date);
+            time.PrintTime();
+            time.now();
+        }
+    }
+}
---
+++ HackTasksWeek3/Tuesday/W3D1_Time/Time.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackWeek3
+{
+    class Time
+    {
+        private DateTime time;
+
+        internal Time(DateTime input)
+        {
+            SaveTime = input;
+        }
+        internal DateTime SaveTime
+            {
+            get { return time; }
+            set { time = value; }
+            }
+        public override string ToString()
+        {
+            string h = time.Hour.ToString();
+            string d = time.Day.ToString();
+            string y = time.Year.ToString();
+            string m = time.Minute.ToString();
+            string s = time.Second.ToString();
+            string mo = time.Month.ToString();
+            string final = h + ":" + m + ":" + s + " " + d + ":" + mo + ":" + y;
+            return final;
+         }
+        internal void PrintTime()
+        {
+            Time timi = new Time(time);
+            Console.WriteLine(timi.ToString());
+        }
+        public void now()
+        {
+            DateTime c =  DateTime.Now;    
+            Time a = new Time(c);
+            Console.WriteLine(a.ToString());   
+        }
+    }
+}
