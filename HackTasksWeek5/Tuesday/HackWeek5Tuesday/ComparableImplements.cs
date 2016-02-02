ed29a3fe54abf088cfdad45a51e927e4e04ccabe
Stefan <stefan_govedarski@abv.bg>
2016-02-03 00:45:50 +0200

    commit

---
+++ HackTasksWeek5/Tuesday/HackWeek5Tuesday/ComparableImplements.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackWeek5Tuesday
+{
+    public class ReverseComparer<T> : IComparer<T>
+    {
+        public int Compare(T firstNum, T secondNum)
+        {
+            return Comparer<T>.Default.Compare(secondNum, firstNum);
+        }
+
+        public class MyIntComparer : IComparer<int>
+        {
+            public int Compare(int firstNum, int secondNum)
+            {
+                return firstNum - secondNum;
+            }
+        }
+        public class StringLengthComparer : IComparer<string>
+        {
+            public int Compare(string firstString, string secondString)
+            {
+                if (firstString == null && secondString == null)
+                    return 0;
+
+                else if (firstString == null)
+                    return 0 - secondString.Length;
+
+                else if (secondString == null)
+                    return firstString.Length - 0;
+
+                else
+                    return firstString.Length - secondString.Length;
+            }
+        }
+
+        public class OddEvenComparer : IComparer<int?>
+        {
+            public int Compare(int? firstNum, int? secondNum)
+            {
+                if (firstNum % 2 == 0 && secondNum % 2 == 1)
+                    return -1;
+                else if (firstNum % 2 == 1 && secondNum % 2 == 0)
+                    return 1;
+                else if (firstNum % 2 == 1 && secondNum % 2 == 1)
+                    return firstNum.Value - secondNum.Value;
+                else if (firstNum % 2 == 0 && secondNum == 0)
+                    return secondNum.Value - firstNum.Value;
+                else
+                {
+                    if (firstNum == null && secondNum == null)
+                        return 0;
+                    else if (firstNum == null)
+                        return -1;
+                    else
+                        return 1;
+                }
+            }
+        }
+        public class LastDigitComparer : IComparer<int>
+        {
+            public int Compare(int firstNum, int secondNum)
+            {
+                return (firstNum % 10) - (secondNum% 10);
+            }
+        }
+    }
+}
---
+++ HackTasksWeek5/Tuesday/HackWeek5Tuesday/Sorting.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackWeek5Tuesday
+{
+    public static class BubbleAndSelectionSort
+    {
+        public static IList<T> BubbleSortin<T>(this IList<T> ListToBeSorted)
+        {
+            return BubbleAndSelectionSort.BubbleSortin(ListToBeSorted,Comparer<T>.Default);
+        }
+        private static IList<T> BubbleSortin<T>(this IList<T> list ,IComparer<T> comparer)
+        {
+            for(int i=0;i<list.Count - 1;i++)
+            {
+                for(int j=0;j<list.Count-i-1;j++)
+                {
+                    if(comparer.Compare(list[j] ,list[j + 1])>0)
+                    {
+                        T temp = list[j];
+                        list[j] = list[j + 1];
+                        list[j + 1] = temp;
+                    }
+                }
+            }
+            return list;
+        }
+   
+        public static IList<T> SelectionSortin<T>(this IList<T> listToBeSorted)
+        {
+            return BubbleAndSelectionSort.SelectionSortin<T>(listToBeSorted, Comparer<T>.Default);
+        }
+        private static IList<T> SelectionSortin<T>(IList<T> list , IComparer<T> comparer)
+        {
+            for(int i=0;i<list.Count -1;i++)
+            {
+                int min = i;
+                for(int j=i+1;j<list.Count;j++ )
+                {
+                    if(comparer.Compare(list[j],list[min])<0)
+                    {
+                        min = j;
+                    }
+                    if(min!=i)
+                    {
+                        T temp = list[j];
+                        list[j] = list[min];
+                        list[min] = temp;
+                    }
+                }
+            }
+            return list;
+        }
+
+
+
+    }
+}
